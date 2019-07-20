68185a2d6679e9b5f989f4b17ad5a548ccfdfbce
Conor Whyte <conor.whyte@ucdconnect.ie>
2019-07-20 01:25:00 +0100

    Border for the estimation cards

--- src/Components/AddEstimation.jsx
+++ src/Components/AddEstimation.jsx
@@ -1,56 +1,49 @@
 import React, { useState } from 'react';
 import { Row, Col, InputNumber, Card, Button } from 'antd';
 
-const cardStyle = {
-  width: 200,
-  textAlign: 'center',
+const cardStyle = (showBorder) => { 
+  return {
+    width: 200,
+    textAlign: 'center',
+    border: showBorder ? '2px solid #3FA9FF' : undefined,
+  };
 };
+
+const estimateCard = (value, setEstimate, currentEstimate) => (
+  <Col span={5} key={value}>
+    <Card
+      title="WAG"
+      hoverable
+      style={cardStyle(currentEstimate === value)}
+      onClick={() => setEstimate(value)}
+    >
+      <h2> {value} </h2>
+    </Card>
+  </Col>
+);
 
 export const AddEstimation = React.memo(props => {
   const [estimate, setEstimate] = useState(0);
+  const estimates = [ 0.5, 0.25, 0.1 ];
 
   return (
     <div className="Estimation-body-cards">
       <h2>{props.storyTitle}</h2>
       <br />
       <Row gutter={16}>
+        
+        {estimates.map(item => (
+          estimateCard(item, setEstimate, estimate)
+        ))}
+        
         <Col span={5}>
-          <Card
-            title="WAG"
-            hoverable
-            style={cardStyle}
-            onClick={() => setEstimate(0.5)}
-          >
-            <h2> 0.5 </h2>
-          </Card>
-        </Col>
-        <Col span={5}>
-          <Card
-            title="WAG"
-            hoverable
-            style={cardStyle}
-            onClick={() => setEstimate(0.25)}
-          >
-            <h2> 0.25 </h2>
-          </Card>
-        </Col>
-        <Col span={5}>
-          <Card
-            title="WAG"
-            hoverable
-            style={cardStyle}
-            onClick={() => setEstimate(0.1)}
-          >
-            <h2> 0.1 </h2>
-          </Card>
-        </Col>
-        <Col span={5}>
-          <Card title="WAG" hoverable style={cardStyle}>
+          <Card title="WAG" hoverable style={cardStyle(false)}>
             <InputNumber onChange={number => setEstimate(number)} />
           </Card>
         </Col>
       </Row>
       <br />
+      
       <Button
         onClick={() => props.sendEstimate(estimate)}
         style={{ margin: 0 }}
